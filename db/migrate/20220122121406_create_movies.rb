3fc09109d297aaf6f1d23f4504eef8991dbbd9fd
Simran Dhaliwal <simran.piers.dhaliwal@gmail.com>
2022-01-22 15:08:12 +0000

    Model logic complete

---
+++ db/migrate/20220122121406_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :overview
+      t.text :poster_url
+      t.string :rating
+      t.string :float
+
+      t.timestamps
+    end
+  end
+end
